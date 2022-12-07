d37f41d2ad528e4df7902605db2dea2959023ccf
LibertyHeise <109040678+LibertyHeise@users.noreply.github.com>
2022-12-06 17:01:20 -0800

    updates

--- SurfsUp/Flask.py
+++ SurfsUp/Flask.py
@@ -1,32 +1,50 @@
 # 1. Import Flask
-from flask import Flask
+from flask import Flask, jsonify
 
 
 # 2. Create an app
 app = Flask(__name__)
 
+# 3. Dictionaries
 
-# 3. Define static routes
+precip_dict = 
+station_dict = 
+temp_dict = 
+specifics_dict = 
+
+# 4. Define static routes
 @app.route("/")
 def index():
-  return "All Available Routes"
+  return f"All Available Routes\
+  precipitation_analysis\
+  stations_analysis\
+  temp_observations\
+  specific_start_data\
+  specific_start_end_data"
 
 
 @app.route("/api/v1.0/precipitation")
-def /api/v1.0/precipitation():
-    name = "Peleke"
-    location = "Tien Shan"
-
-    return f"My name is {name}, and I live in {location}."
+def precipitation_analysis():
+    return f"Here is the JSON representation of the dictionary."
 
 
-@app.route("/contact")
-def contact():
-    email = "peleke@exple.com"
+@app.route("/api/v1.0/stations")
+def stations_analysis():
+    return f"Here is the JSON list of stations."
+    
+@app.route("/api/v1.0/tobs")
+def temp_observations():
+    return f"Here is a JSON list of temp obs. for the previous year."
+    
+@app.route("/api.v1.0/<start>")
+def specific_start_data():
+    return f"JSON list of all minimum, avg, max temps"
 
-    return f"Questions? Comments? Complaints? Shoot an email to {email}."
+@app.route("/api/v1.0/<start>/<end>")
+def specific_start_end_data():
+    return f"specifics"
 
 
-# 4. Define main behavior
+# 5. Define main behavior
 if __name__ == "__main__":
     app.run(debug=True)
